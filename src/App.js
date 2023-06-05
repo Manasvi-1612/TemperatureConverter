802d6ee6b046221f8b7903b4894b58a2f34962ee
Manasvi <manasvijoshi63@gmail.com>
2023-06-05 22:22:07 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Input from "./Input";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
+  <>
+  
+    <Router>
+
     <div className="App">
-      <Input/>
+      <Routes>
+
+      <Route exact path="/" element={<Input/>}/>
+      <Route exact path="/TemperatureConverter" element={<Input/>}/>
+ 
+      </Routes>
     </div>
+    </Router>
+  </>
   );
 }
 
